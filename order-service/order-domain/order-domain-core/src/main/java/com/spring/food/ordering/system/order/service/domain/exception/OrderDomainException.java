9f29f506237765067801116df1368650c838d303
Duc Phan <57104807+dkevin96@users.noreply.github.com>
2024-10-17 20:09:23 +0200

    Add Order, OrderItem Entity implementation methods

---
+++ order-service/order-domain/order-domain-core/src/main/java/com/spring/food/ordering/system/order/service/domain/exception/OrderDomainException.java
@@ -0,0 +1,14 @@
+package com.spring.food.ordering.system.order.service.domain.exception;
+
+import com.spring.food.ordering.system.domain.exception.DomainException;
+
+public class OrderDomainException extends DomainException {
+
+  public OrderDomainException(String message) {
+    super(message);
+  }
+
+  public OrderDomainException(String message, Throwable cause) {
+    super(message, cause);
+  }
+}
