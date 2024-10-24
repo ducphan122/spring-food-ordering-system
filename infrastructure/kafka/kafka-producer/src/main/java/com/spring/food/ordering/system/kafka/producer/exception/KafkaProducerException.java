60a22ff0fc477993fe6992fe85c53bbe99776708
Duc Phan <57104807+dkevin96@users.noreply.github.com>
2024-10-24 12:34:19 +0200

    Implement kafka-producer generic module

---
+++ infrastructure/kafka/kafka-producer/src/main/java/com/spring/food/ordering/system/kafka/producer/exception/KafkaProducerException.java
@@ -0,0 +1,8 @@
+package com.spring.food.ordering.system.kafka.producer.exception;
+
+public class KafkaProducerException extends RuntimeException {
+
+  public KafkaProducerException(String message) {
+    super(message);
+  }
+}
