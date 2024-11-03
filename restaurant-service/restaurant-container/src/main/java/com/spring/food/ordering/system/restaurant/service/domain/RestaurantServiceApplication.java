496d2cae15fd18e20acc6fa76572a2f684aa05c9
Duc Phan <57104807+dkevin96@users.noreply.github.com>
2024-11-03 22:10:53 +0100

    Update restaurant-container

--- restaurant-service/restaurant-container/src/main/java/com/spring/food/ordering/system/restaurant/service/domain/RestaurantServiceApplication.java
+++ restaurant-service/restaurant-container/src/main/java/com/spring/food/ordering/system/restaurant/service/domain/RestaurantServiceApplication.java
@@ -5,10 +5,16 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@EnableJpaRepositories(basePackages = { "com.spring.food.ordering.system.restaurant.service.dataaccess",
-        "com.spring.food.ordering.system.dataaccess" })
-@EntityScan(basePackages = { "com.spring.food.ordering.system.restaurant.service.dataaccess",
-        "com.spring.food.ordering.system.dataaccess" })
+@EnableJpaRepositories(
+        basePackages = {
+            "com.spring.food.ordering.system.restaurant.service.dataaccess",
+            "com.spring.food.ordering.system.dataaccess"
+        })
+@EntityScan(
+        basePackages = {
+            "com.spring.food.ordering.system.restaurant.service.dataaccess",
+            "com.spring.food.ordering.system.dataaccess"
+        })
 @SpringBootApplication(scanBasePackages = "com.spring.food.ordering.system")
 public class RestaurantServiceApplication {
     public static void main(String[] args) {
