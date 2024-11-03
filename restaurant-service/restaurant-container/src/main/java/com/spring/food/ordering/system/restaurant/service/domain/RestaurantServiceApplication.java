18a1abe1764516ebb3dc299ca7d939fc95b2e524
Duc Phan <57104807+dkevin96@users.noreply.github.com>
2024-11-03 22:10:04 +0100

    Add restaurant-container

--- restaurant-service/restaurant-container/src/main/java/com/spring/food/ordering/system/restaurant/service/domain/RestaurantServiceApplication.java
+++ restaurant-service/restaurant-container/src/main/java/com/spring/food/ordering/system/restaurant/service/domain/RestaurantServiceApplication.java
@@ -2,7 +2,13 @@ package com.spring.food.ordering.system.restaurant.service.domain;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+@EnableJpaRepositories(basePackages = { "com.spring.food.ordering.system.restaurant.service.dataaccess",
+        "com.spring.food.ordering.system.dataaccess" })
+@EntityScan(basePackages = { "com.spring.food.ordering.system.restaurant.service.dataaccess",
+        "com.spring.food.ordering.system.dataaccess" })
 @SpringBootApplication(scanBasePackages = "com.spring.food.ordering.system")
 public class RestaurantServiceApplication {
     public static void main(String[] args) {
--- restaurant-service/restaurant-messaging/src/main/java/com/spring/food/ordering/system/restaurant/service/messaging/listener/kafka/RestaurantApprovalRequestKafkaListener.java
+++ restaurant-service/restaurant-messaging/src/main/java/com/spring/food/ordering/system/restaurant/service/messaging/listener/kafka/RestaurantApprovalRequestKafkaListener.java
@@ -4,14 +4,13 @@ import com.spring.food.ordering.system.kafka.consumer.KafkaConsumer;
 import com.spring.food.ordering.system.kafka.order.avro.model.RestaurantApprovalRequestAvroModel;
 import com.spring.food.ordering.system.restaurant.service.domain.ports.input.message.listener.RestaurantApprovalRequestMessageListener;
 import com.spring.food.ordering.system.restaurant.service.messaging.mapper.RestaurantMessagingDataMapper;
+import java.util.List;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.support.KafkaHeaders;
 import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
-
-import java.util.List;
 
 @Slf4j
 @Component
@@ -20,23 +19,25 @@ public class RestaurantApprovalRequestKafkaListener implements KafkaConsumer<Res
     private final RestaurantApprovalRequestMessageListener restaurantApprovalRequestMessageListener;
     private final RestaurantMessagingDataMapper restaurantMessagingDataMapper;
 
-    public RestaurantApprovalRequestKafkaListener(RestaurantApprovalRequestMessageListener
-                                                          restaurantApprovalRequestMessageListener,
-                                                  RestaurantMessagingDataMapper
-                                                          restaurantMessagingDataMapper) {
+    public RestaurantApprovalRequestKafkaListener(
+            RestaurantApprovalRequestMessageListener restaurantApprovalRequestMessageListener,
+            RestaurantMessagingDataMapper restaurantMessagingDataMapper) {
         this.restaurantApprovalRequestMessageListener = restaurantApprovalRequestMessageListener;
         this.restaurantMessagingDataMapper = restaurantMessagingDataMapper;
     }
 
     @Override
-    @KafkaListener(id = "${kafka-consumer-config.restaurant-approval-consumer-group-id}",
+    @KafkaListener(
+            id = "${kafka-consumer-config.restaurant-approval-consumer-group-id}",
             topics = "${restaurant-service.restaurant-approval-request-topic-name}")
-    public void receive(@Payload List<RestaurantApprovalRequestAvroModel> messages,
-                        @Header(KafkaHeaders.RECEIVED_KEY) List<String> keys,
-                        @Header(KafkaHeaders.RECEIVED_PARTITION) List<Integer> partitions,
-                        @Header(KafkaHeaders.OFFSET) List<Long> offsets) {
-        log.info("{} number of orders approval requests received with keys {}, partitions {} and offsets {}" +
-                        ", sending for restaurant approval",
+    public void receive(
+            @Payload List<RestaurantApprovalRequestAvroModel> messages,
+            @Header(KafkaHeaders.RECEIVED_KEY) List<String> keys,
+            @Header(KafkaHeaders.RECEIVED_PARTITION) List<Integer> partitions,
+            @Header(KafkaHeaders.OFFSET) List<Long> offsets) {
+        log.info(
+                "{} number of orders approval requests received with keys {}, partitions {} and offsets {}"
+                        + ", sending for restaurant approval",
                 messages.size(),
                 keys.toString(),
                 partitions.toString(),
@@ -44,9 +45,9 @@ public class RestaurantApprovalRequestKafkaListener implements KafkaConsumer<Res
 
         messages.forEach(restaurantApprovalRequestAvroModel -> {
             log.info("Processing order approval for order id: {}", restaurantApprovalRequestAvroModel.getOrderId());
-            restaurantApprovalRequestMessageListener.approveOrder(restaurantMessagingDataMapper.
-                    restaurantApprovalRequestAvroModelToRestaurantApproval(restaurantApprovalRequestAvroModel));
+            restaurantApprovalRequestMessageListener.approveOrder(
+                    restaurantMessagingDataMapper.restaurantApprovalRequestAvroModelToRestaurantApproval(
+                            restaurantApprovalRequestAvroModel));
         });
     }
-
 }
