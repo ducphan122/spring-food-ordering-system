2a4b59c363e2ea0716f4fd075f0220565a70203f
Duc Phan <57104807+dkevin96@users.noreply.github.com>
2024-11-03 12:54:34 +0100

    Implement payment-dataacess module

---
+++ payment-service/payment-dataaccess/src/main/java/com/spring/food/ordering/system/payment/service/dataaccess/credithistory/exception/CreditHistoryDataaccessException.java
@@ -0,0 +1,8 @@
+package com.spring.food.ordering.system.payment.service.dataaccess.credithistory.exception;
+
+public class CreditHistoryDataaccessException extends RuntimeException {
+
+    public CreditHistoryDataaccessException(String message) {
+        super(message);
+    }
+}
