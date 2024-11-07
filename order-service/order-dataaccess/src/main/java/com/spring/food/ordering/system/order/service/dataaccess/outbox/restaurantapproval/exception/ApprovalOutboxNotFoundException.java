08e4cc4105bfb66262b133cf63efbf5b16501f44
Duc Phan <57104807+dkevin96@users.noreply.github.com>
2024-11-07 01:16:44 +0100

    Refactore Order Data Access module for Outbox pattern

---
+++ order-service/order-dataaccess/src/main/java/com/spring/food/ordering/system/order/service/dataaccess/outbox/restaurantapproval/exception/ApprovalOutboxNotFoundException.java
@@ -0,0 +1,8 @@
+package com.spring.food.ordering.system.order.service.dataaccess.outbox.restaurantapproval.exception;
+
+public class ApprovalOutboxNotFoundException extends RuntimeException {
+
+    public ApprovalOutboxNotFoundException(String message) {
+        super(message);
+    }
+}
