a84943a12335fd5eed669c08f5bea45c3fbe3307
Duc Phan <57104807+dkevin96@users.noreply.github.com>
2024-11-10 15:00:14 +0100

    Add pessimistic for credit entry

--- payment-service/payment-dataaccess/src/main/java/com/spring/food/ordering/system/payment/service/dataaccess/creditentry/repository/CreditEntryJpaRepository.java
+++ payment-service/payment-dataaccess/src/main/java/com/spring/food/ordering/system/payment/service/dataaccess/creditentry/repository/CreditEntryJpaRepository.java
@@ -1,13 +1,16 @@
 package com.spring.food.ordering.system.payment.service.dataaccess.creditentry.repository;
 
 import com.spring.food.ordering.system.payment.service.dataaccess.creditentry.entity.CreditEntryEntity;
+import jakarta.persistence.LockModeType;
 import java.util.Optional;
 import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface CreditEntryJpaRepository extends JpaRepository<CreditEntryEntity, UUID> {
 
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
     Optional<CreditEntryEntity> findByCustomerId(UUID customerId);
 }
